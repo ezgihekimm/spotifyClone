a840f601069f64236ac16b0e995516f4a7ef8f60
ezgi.hekim.2000 <ezgi.hekim.2000@gmail.com>
2024-02-05 15:47:19 +0300

    Initial Commit

---
+++ Deneme/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Deneme
+//
+//  Created by Ezgi Hekim on 5.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Deneme/DenemeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DenemeApp.swift
+//  Deneme
+//
+//  Created by Ezgi Hekim on 5.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct DenemeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
